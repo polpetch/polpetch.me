c8f8c823a9c848bba4e696f15ad953b4eb050cc4
oat <oat@oats-macbook-air.local>
2022-09-20 00:36:01 +0700

    just change something

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://polpetch.me">Polpetch.me</a>
+          <a href="https://polpetch.me">Polpetch.me</a>
         </h1>
 
         <p className={styles.description}>
