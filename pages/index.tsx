e3e8ef8e64a4f64ddb0d1a36500e42183cc61669
oat <oat@oats-macbook-air.local>
2022-09-20 15:43:48 +0700

    now we have our own favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>Polpetch.me</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚧</text></svg>"></link>
       </Head>
 
       <main className={styles.main}>
