ce25515f1f61ce6b3f0d178e1f360ec4a7a81838
oat <oat@oats-macbook-air.local>
2022-10-09 01:30:57 +0700

    remove dark theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Polpetch.me</title>
+        <title>Portfolio</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>"></link>
       </Head>
 
