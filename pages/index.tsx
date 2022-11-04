62235504c08120c815c609ccab3fc24836863bff
oat <oat@oats-macbook-air.local>
2022-11-04 23:38:32 +0700

    made more polish

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,33 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 const Home: NextPage = () => {
+   
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Portfolio</title>
+        <title>Polpetch - Portfolio</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>"></link>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title} style={{fontSize: '2rem'}}>
+        <h1 className={styles.title} style={{fontSize: '2rem', borderBottom: '.9px solid #00000074' }}>
           <a style={{ color: '#FF7D63'}} >Polpetch Prasompetch</a>
           <a href='https://resume.showwcase.com/1998polpetch.pdf' style={{ float: 'right', cursor: 'pointer' }} >&rarr; Resume</a>
         </h1>
 
-        <div style={{  }}>
-          <p>test</p>
-          <p className={styles.description}>
-            (beginner) Web Developer 
-          </p>
-        </div>
+        
 
-        <div className={styles.grid}>
-          <a href="https://github.com/polpetch/milk-domain-name-service" className={styles.card}>
-            <h2>.Milk Domain Name services  &rarr;</h2>
+        <div className={styles.grid} style={{ float: 'left'}} >
+          <a href="https://github.com/polpetch/milk-domain-name-service" className={styles.card}  style={{ cursor: 'pointer' }}>
+            <h2>.Milk Domain Name services 🥛</h2>
             <p>Polygon chain domain name service</p>
           </a>
-
-          <a href="https://polpetch.me" className={styles.card}>
-            <h2>And this one for black cat &rarr;</h2>
-            <p>🐈‍⬛</p>
-          </a>
-          <a href="https://polpetch.me" className={styles.card}>
-            <h2>And this one for black cat &rarr;</h2>
-            <p>🐈‍⬛</p>
-         </a>
         </div>
       </main>
 
@@ -46,7 +36,7 @@ const Home: NextPage = () => {
         <a style={{ float: 'right' }}>polpetch@icloud.com</a>
         <br></br>
         <a>Pathum Thani, Thailand</a> 
-        <a style={{ float: 'right', paddingRight: '46px' }}>+66 8 657 6479</a>
+        <a style={{ float: 'right', paddingRight: '46px' }}>+66 0 657 6479</a>
       </footer>
     </div>
   )
