abf9b73235c1568efc74b66ee31a9f5b3d574d03
oat <oat@oats-MacBook-Air.local>
2022-12-20 23:42:38 +0700

    change exp order

--- components/body.tsx
+++ components/body.tsx
@@ -8,26 +8,6 @@ export default function Body() {
   return (
     <div className={styles.body}>
       <p className={styles.index}> EXPREIENCES </p>
-
-      <hr/>
-      <div className={styles.post}>
-        <div className={styles.imageWraper}>
-          <Image 
-            src={MilkDomain} 
-            alt='Picture of .milk domain name service web site' 
-            objectFit='fill'
-          />
-        </div>
-        <div className={styles.content} >
-          <p >2022</p>
-          <p></p>
-          <a href='https://github.com/polpetch/milk-domain-name-service' >.Milk Domain Name Service</a>
-          <div>
-            <p>A domain name service based on polygon testnet network build with nextjs and solidity. </p>
-          </div>
-        </div>
-        {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta neque ipsa facilis animi inventore. Placeat dolore voluptate, doloremque amet neque in possimus maxime consequuntur eaque alias, dicta ullam, odio inventore.</p> */}
-      </div>
       <hr/>
       <div className={styles.post}>
         <div className={styles.imageWraper}>
@@ -47,7 +27,25 @@ export default function Body() {
             </p>
           </div>
         </div>
-        {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta neque ipsa facilis animi inventore. Placeat dolore voluptate, doloremque amet neque in possimus maxime consequuntur eaque alias, dicta ullam, odio inventore.</p> */}
+      </div>
+      <hr/>
+      
+      <div className={styles.post}>
+        <div className={styles.imageWraper}>
+          <Image 
+            src={MilkDomain} 
+            alt='Picture of .milk domain name service web site' 
+            objectFit='fill'
+          />
+        </div>
+        <div className={styles.content} >
+          <p >2022</p>
+          <p></p>
+          <a href='https://github.com/polpetch/milk-domain-name-service' style={{ cursor: 'pointer' }} >.Milk Domain Name Service</a>
+          <div>
+            <p>A domain name service based on polygon testnet network build with nextjs and solidity. </p>
+          </div>
+        </div>
       </div>
       <hr/>
     </div>
